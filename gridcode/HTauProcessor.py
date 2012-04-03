a6daa2e1fa841cc8e215f11ba5a262b7f3365ff7
end <end@4525493e-7705-40b1-a816-d608a930855b>
2012-04-03 14:54:14 +0000

    do MMC
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasgrp/Physics/Higgs/HSG4/software/common/higgspy/trunk@280090 4525493e-7705-40b1-a816-d608a930855b

--- gridcode/HTauProcessor.py
+++ gridcode/HTauProcessor.py
@@ -206,7 +206,7 @@ class HTauProcessor(ATLASStudent):
             """
             MMC and misc variables
             """
-            #D4PD.MMC_mass = missingmass.mass(taus, jets, METx, METy, sumET, self.fileset.datatype)
+            D4PD.MMC_mass = missingmass.mass(taus, jets, METx, METy, sumET, self.fileset.datatype)
             D4PD.Mvis_tau1_tau2 = utils.Mvis(taus[0].Et, taus[0].seedCalo_phi, taus[1].Et, taus[1].seedCalo_phi)
             D4PD.numVertices = len([vtx for vtx in event.vertices if (vtx.type == 1 and vtx.nTracks >= 4) or (vtx.type == 3 and vtx.nTracks >= 2)])
             D4PD.numJets = len(jets)
