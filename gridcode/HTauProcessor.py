aa2bc70384b0322293416fb9833ce7f37cf2a877
end <end@4525493e-7705-40b1-a816-d608a930855b>
2012-04-03 14:54:45 +0000

    updates
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasgrp/Physics/Higgs/HSG4/software/common/higgspy/trunk@280098 4525493e-7705-40b1-a816-d608a930855b

--- gridcode/HTauProcessor.py
+++ gridcode/HTauProcessor.py
@@ -82,11 +82,11 @@ class HTauProcessor(ATLASStudent):
         tree.set_filters(self.event_filters)
 
         # define tree collections
-        tree.define_collection(name="taus", prefix="tau_", size="tau_n", mixin=TauFourMomentum)
+        tree.define_collection(name="taus", prefix="tau_", size="tau_n", mix=TauFourMomentum)
         # jet_eta etc is AntiKt4LCTopo in tau-perf D3PDs
-        tree.define_collection(name="jets", prefix="jet_AntiKt4TopoEM_", size="jet_AntiKt4TopoEM_n", mixin=FourMomentum)
+        tree.define_collection(name="jets", prefix="jet_AntiKt4TopoEM_", size="jet_AntiKt4TopoEM_n", mix=FourMomentum)
         tree.define_collection(name="truetaus", prefix="trueTau_", size="trueTau_n")
-        tree.define_collection(name="mc", prefix="mc_", size="mc_n", mixin=MCParticle)
+        tree.define_collection(name="mc", prefix="mc_", size="mc_n", mix=MCParticle)
         tree.define_collection(name="muons", prefix="mu_staco_", size="mu_staco_n")
         tree.define_collection(name="electrons", prefix="el_", size="el_n")
         tree.define_collection(name="vertices", prefix="vxp_", size="vxp_n")
--- gridcode/test.sh
+++ gridcode/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#grid-batch --dataset data --metadata datasets.yml --student HTauProcessor ../testdata/data
+grid-batch --dataset data --metadata datasets.yml --student HTauProcessor ../../testdata/data
 #grid-batch --dataset VBFH130hh --metadata SM_datasets.yml --student HTauProcessor ../../testdata/VBFH130_tautauhh
-grid-batch --dataset VBFH100hh --metadata datasets.yml --student HTauProcessor --events 1000 ../../testdata/Ztautau
+#grid-batch --dataset VBFH100hh --metadata datasets.yml --student HTauProcessor --events 1000 ../../testdata/Ztautau
