2286591ab1e1751edc62815f3f76cf975a08e37d
end <end@4525493e-7705-40b1-a816-d608a930855b>
2012-04-03 15:05:50 +0000

    updates
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasgrp/Physics/Higgs/HSG4/software/common/higgspy/trunk@280238 4525493e-7705-40b1-a816-d608a930855b

--- gridcode/HTauSkim.py
+++ gridcode/HTauSkim.py
@@ -1,3 +1,57 @@
+"""
+==================
+Skimming procedure
+==================
+
+A) For DATA skimming:
+
+1) Trigger
+
+    The lowest un-prescaled trigger can be found in
+    https://twiki.cern.ch/twiki/bin/viewauth/Atlas/LowestUnprescaled
+
+    - Period A-I  :  tau29_medium1_tau20_medium1 || tau100_medium || xe60_noMu
+    - Period J    :  tau29_medium1_tau20_medium1 || tau100_medium || xe60_tight_noMu
+    - Period K    : tau29_medium1_tau20_medium1 || tau125_medium1 || xe60_tight_noMu
+    - Period L-M  : tau29T_medium1_tau20T_medium1 || tau125_medium1 || xe60_verytight_noMu
+
+
+2) Two LOOSE tau
+
+    - tau_author!=2 && tau_pT > 18 GeV && tau_numTrack > 0
+    - tau_JetBDTLoose==1 || tau_tauLlhLoose==1
+
+    * Note pT>18GeV cut was chosen to be able to fluctuate the TES.
+
+    With these two selection (1 & 2), we expect factor 50 reduction which
+    results in ~400-500 GB disk space at 5 fb-1.
+
+
+B) For MC skimming:
+
+1) Trigger : OR of above.
+
+==========
+Histograms
+==========
+
+Some information has to be kept as "histogram" during the skimming.
+
+Just after "Trigger" requirement, the histograms should be made according
+to each trigger decision.  (three histograms for each variable)
+
+ * number of events with GRL (to check consistency of LumiCalc)
+ * mu-distribution
+ * # of vertex
+ * # of LOOSE tau (pt>18GeV, LLH||BDT-loose)
+ * # of EF tau trigger object  (no matching is necessary)
+ * tau pT spectrum
+ * EF tau pT spectrum  (no matching is necessary)
+ * MET
+ * anything else???
+"""
+
+
 import ROOT
 from atlastools import utils
 from atlastools.units import *
@@ -25,8 +79,8 @@ class TwoGoodLooseTaus(EventFilter):
 
     def passes(self, event):
 
-        event.taus.select(lambda tau: tau.author != 2 and tau.seedCalo_numTrack > 0 and
-                                      tau.pt > 20*GeV and (tau.tauLlhLoose == 1 or tau.JetBDTSigLoose == 1))
+        event.taus.select(lambda tau: tau.author != 2 and tau.numTrack > 0 and
+                                      tau.pt > 18*GeV and (tau.tauLlhLoose == 1 or tau.JetBDTSigLoose == 1))
         return len(event.taus) > 1
 
 
@@ -80,3 +134,6 @@ class HTauSkim(ATLASStudent):
         for event in intree:
             
             outtree.Fill()
+
+        outtree.FlushBaskets()
+        outtree.Write()
