58994af3ff358209660247a74c08ea4f8d87cff6
end <end@4525493e-7705-40b1-a816-d608a930855b>
2012-04-03 14:58:31 +0000

    updates
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasgrp/Physics/Higgs/HSG4/software/common/higgspy/trunk@280146 4525493e-7705-40b1-a816-d608a930855b

--- gridcode/test.sh
+++ gridcode/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-grid-batch --dataset data --metadata datasets.yml --student HTauProcessor --events 1000 ../../testdata/rel17
+grid-batch --dataset data --metadata rel17_datasets.yml --student HTauProcessor --events 1000 ../../testdata/rel17data
 
 grid-batch --dataset data --metadata datasets.yml --student HTauProcessor --events 1000 ../../testdata/data
 #grid-batch --dataset VBFH130hh --metadata SM_datasets.yml --student HTauProcessor ../../testdata/VBFH130_tautauhh
