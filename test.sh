0832fe290a92aa32332780059897081ddb286af5
end <end@4525493e-7705-40b1-a816-d608a930855b>
2012-04-03 14:39:48 +0000

    updates
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasgrp/Physics/Higgs/HSG4/software/common/higgspy/trunk@279905 4525493e-7705-40b1-a816-d608a930855b

--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#grid-batch --dataset data --metadata htau_datasets.yml --student HTauProcessor ../testdata/data
+grid-batch --dataset data --metadata datasets.yml --student HTauProcessor ../testdata/data
 
-grid-batch --dataset VBFH100hh --metadata datasets.yml --student HTauProcessor ../testdata/Ztautau
-
-#grid-batch --dataset Ztautau25 --metadata tauid_datasets.yml --student TauIDProcessor ../testdata/Ztautau
+#grid-batch --dataset VBFH100hh --metadata datasets.yml --student HTauProcessor ../testdata/Ztautau
