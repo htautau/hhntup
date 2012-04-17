1a74b9b3ffffcee8db4fee7dfed3b46daf77f7c1
atanasi <atanasi@4525493e-7705-40b1-a816-d608a930855b>
2012-04-17 13:37:19 +0000

    Add run_data_eh.py script for eh data
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasgrp/Physics/Higgs/HSG4/software/common/higgspy/trunk@284832 4525493e-7705-40b1-a816-d608a930855b

---
+++ run_data_eh.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import cluster
+
+
+hosts = cluster.get_hosts('hosts.sfu.txt')
+setup = cluster.get_setup('setup.andres.sfu.txt')
+
+datasets = [
+'data'
+]
+
+cluster.run('eLHProcessor.py',
+            datasets=datasets,
+            hosts=hosts,
+            nproc=8,
+            nice=10,
+            setup=setup)
