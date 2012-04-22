f1515278f219b67b17cbd7c5d768c72a9092b00e
mtm <mtm@4525493e-7705-40b1-a816-d608a930855b>
2012-04-22 14:12:17 +0000

    Cleaning up filters in lephad
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasgrp/Physics/Higgs/HSG4/software/common/higgspy/trunk@286537 4525493e-7705-40b1-a816-d608a930855b

--- higgstautau/lephad/filters.py
+++ higgstautau/lephad/filters.py
@@ -350,6 +350,45 @@ class ElectronVeto(EventFilter):
        return True
 
 
+############################################################
+# TAU SELECTION
+############################################################
+
+def tau_skimselection(tau):
+    """ Does the complete tau preselection """
+
+    if not (tau.pt > 15*GeV) : return False
+    if not (tau.numTrack == 1 or tau.numTrack == 3) : return False
+    if not (abs(tau.eta) < 2.5) : return False
+
+    return True
+
+
+def tau_preselection(tau):
+    """ Does the complete tau preselection """
+
+    if not (tau.pt > 20*GeV) : return False
+    if not (tau.numTrack == 1 or tau.numTrack == 3) : return False
+    if not (tau.JetBDTSigMedium == 1) : return False
+    if not (abs(tau.eta) < 2.5) : return False
+    if not (tau.author != 2) : return False
+    if not (abs(tau.charge) == 1) : return False
+    if not (tau.EleBDTMedium == 0) : return False
+    if not (tau.muonVeto == 0) : return False
+
+    return True
+
+
+def tau_selection(tau):
+    """ Finalizes the tau selection """
+
+    return True
+
+
+############################################################
+# MUON SELECTION
+############################################################
+
 def muon_has_good_track(muon):
 
     blayer = (muon.expectBLayerHit == 0.0) or (muon.nBLHits > 0)
@@ -368,33 +407,12 @@ def muon_has_good_track(muon):
     return blayer and pix and sct and holes and trt
 
 
-def tau_preselection(tau):
-    """ Does the complete tau preselection """
+def muon_skimselection(mu):
+    """ Does the complete muon preselection """
 
-    if not (tau.pt > 20*GeV) : return False
-    if not (tau.numTrack == 1 or tau.numTrack == 3) : return False
-    if not (tau.JetBDTSigMedium == 1) : return False
-    if not (abs(tau.eta) < 2.5) : return False
-    if not (tau.author != 2) : return False
-    if not (abs(tau.charge) == 1) : return False
-    if not (tau.EleBDTMedium == 0) : return False
-    if not (tau.muonVeto == 0) : return False
-
-    return True
-
-
-def tau_skimselection(tau):
-    """ Does the complete tau preselection """
-
-    if not (tau.pt > 15*GeV) : return False
-    if not (tau.numTrack == 1 or tau.numTrack == 3) : return False
-    if not (abs(tau.eta) < 2.5) : return False
-
-    return True
-
-
-def tau_selection(tau):
-    """ Finalizes the tau selection """
+    if not (mu.pt > 13*GeV) : return False
+    if not (abs(mu.eta) < 3.0) : return False
+    if not (mu.loose) : return False
 
     return True
 
@@ -410,32 +428,32 @@ def muon_preselection(mu):
     return True
 
 
-def muon_skimselection(mu):
-    """ Does the complete muon preselection """
-
-    if not (mu.pt > 13*GeV) : return False
-    #if not (muon_has_good_track(mu)) : return False
-    if not (abs(mu.eta) < 3.0) : return False
-    if not (mu.loose) : return False
-
-    return True
-
-
 def muon_selection(mu):
     """ Finalizes the muon selection """
 
     if not (mu.isCombinedMuon) : return False
-    if not (mu.EFMG_matched) : return False
+    if not (mu.pt > 20*GeV) : return False
 
-    # Find out if the muon is matched to a good trigger muon
-    nMatches = mu.EFMG_n
-    goodMatch = False
-    for i in range(0, nMatches):
-        if mu.EFMG_pt[i] > 18000: goodMatch = True
-
-    return goodMatch
+    return True
 
 
+############################################################
+# ELECTRON SELECTION
+############################################################
+
+def electron_skimselection(e):
+    """ Does the complete electron preselection """
+
+    cl_eta = e.cl_eta
+    trk_eta = e.tracketa
+    cl_Et = e.cl_E/cosh(trk_eta)
+    
+    if not (cl_Et > 15*GeV) : return False
+    if not (abs(cl_eta) < 3.0) : return False
+    if not (e.author == 1 or e.author == 3) : return False
+    if not (e.mediumPP) : return False
+
+    return True
 
 
 def electron_preselection(e):
@@ -454,30 +472,22 @@ def electron_preselection(e):
     return True
 
 
-def electron_skimselection(e):
-    """ Does the complete electron preselection """
+def electron_selection(e):
+    """ Finalizes the electron selection"""
 
     cl_eta = e.cl_eta
     trk_eta = e.tracketa
     cl_Et = e.cl_E/cosh(trk_eta)
-    
-    if not (cl_Et > 15*GeV) : return False
-    if not (abs(cl_eta) < 3.0) : return False
-    #if (1.37 < abs(cl_eta) < 1.52) : return False
-    if not (e.author == 1 or e.author == 3) : return False
-    if not (e.mediumPP) : return False
 
-    return True
-
-
-def electron_selection(e):
-    """ Finalizes the electron selection"""
-
-    if not (cl_Et > 20*GeV) : return False
+    if not (cl_Et > 25*GeV) : return False
     if not (e.tightPP) : return False
 
     return True
 
+
+############################################################
+# JET SELECTION
+############################################################
 
 def jet_preselection(jet):
     """ Does the complete jet preselection """
@@ -498,11 +508,20 @@ def jet_selection(jet):
     return True
 
 
+############################################################
+# VERTEX SELECTION
+############################################################
+
 def vertex_selection(vxp):
     """ Does the full primary and pileup vertex selection """
 
     return (vxp.type == 1 and vxp.nTracks >= 4) or (vxp.type == 3 and vxp.nTracks >= 2)
 
+
+
+############################################################
+# OBJECT ANALYSIS FILTERS
+############################################################
 
 class MuonPreSelection(EventFilter):
     """Selects muons of good quality"""
