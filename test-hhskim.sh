52c20c4b51f792a79d189f7f28dad2b6fa5e4806
end <end@4525493e-7705-40b1-a816-d608a930855b>
2012-04-22 01:17:01 +0000

    updates
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasgrp/Physics/Higgs/HSG4/software/common/higgspy/trunk@286472 4525493e-7705-40b1-a816-d608a930855b

--- test-hhskim.sh
+++ test-hhskim.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-grid-batch --dataset Ztautau_hadhad --metadata datasets.cfg --student HHSkim /global/endw/testdata/mc11c
+grid-batch --dataset Ztautau_hadhad --metadata datasets.cfg --student HHSkim /global/endw/mc11_7TeV/mmc_samples/hh/mc11_7TeV.125205.PowHegPythia_VBFH125_tautauhh.merge.NTUP_TAUMEDIUM.e893_s1310_s1300_r3043_r2993_p851_tid654029_00
 #grid-batch --dataset data_hadhad --metadata datasets.cfg --student HHSkim /global/endw/testdata/newrel17/data
