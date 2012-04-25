e5db28a656932c542521d10bf05fea15a3dfe140
end <end@4525493e-7705-40b1-a816-d608a930855b>
2012-04-25 09:44:17 +0000

    updates
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasgrp/Physics/Higgs/HSG4/software/common/higgspy/trunk@287868 4525493e-7705-40b1-a816-d608a930855b

--- post_ntuples.py
+++ post_ntuples.py
@@ -4,7 +4,9 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser()
 parser.add_argument('-u', '--user')
-parser.add_argument('dir')
+parser.add_argument('-d', '--dir')
+parser.add_argument('-v', '--verbose', action='store_true', default=False)
+parser.add_argument('files', nargs='*')
 args = parser.parse_args()
 
 from glob import glob
@@ -14,4 +16,13 @@ import subprocess
 AFS_WORKSPACE = '/afs/cern.ch/work/%s/%s/public' % (args.user[0], args.user)
 HOST = '%s@lxplus5.cern.ch' % args.user
 
-subprocess.call(['scp'] + glob(os.path.join(args.dir, '*.root')) + [':'.join([HOST, AFS_WORKSPACE])])
+if args.dir:
+    AFS_WORKSPACE = os.path.join(AFS_WORKSPACE, args.dir)
+
+def cmd(args, verbose=args.verbose):
+
+    if verbose:
+        print ' '.join(args)
+    subprocess.call(args)
+
+cmd(['scp'] + args.files + [':'.join([HOST, AFS_WORKSPACE])])
