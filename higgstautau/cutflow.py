d3ae413d298e149496a51aa58bdbb6a95c1f9094
Noel Dawe <Noel.Dawe@cern.ch>
2012-05-10 23:22:04 -0700

    better cutflow table format

--- higgstautau/cutflow.py
+++ higgstautau/cutflow.py
@@ -3,7 +3,7 @@ from argparse import ArgumentParser
 from higgstautau import datasets
 from higgstautau.hadhad import periods
 from higgstautau.hadhad.periods import total_lumi
-from tabulartext import PrettyTable
+from tabulartext import PrettyTable, TextTable
 from rootpy.io import open as ropen
 import math
 import os
@@ -13,13 +13,20 @@ def get_parser():
 
     parser = ArgumentParser()
     parser.add_argument('--format', choices=('latex', 'text'), default='text')
-    parser.add_argument('--short', action='store_true', default=False)
+    parser.add_argument('--errors', action='store_true', default=False)
+    parser.add_argument('--precision', default=2)
     parser.add_argument('--proc', default='HHProcessor')
     parser.add_argument('--db')
     parser.add_argument('--noweight', action='store_true', default=False)
     parser.add_argument('--verbose', action='store_true', default=False)
-    #parser.add_argument('--dir', default='samples/midpt')
+    parser.add_argument('--rst', action='store_true', default=False)
+    parser.add_argument('--rst-class', default='cutflow')
     return parser
+
+
+def tab(s, tabstr=4 * ' '):
+
+    return '\n'.join(((tabstr) + x if x else x) for x in s.splitlines())
 
 
 def make_cutflow(samples,
@@ -35,6 +42,7 @@ def make_cutflow(samples,
 
     # build table
     cutflow_table = {}
+    data_index = []
     for i, (latex_name, text_name, sample) in enumerate(samples):
         matched_samples = db.search(sample)
         if not matched_samples:
@@ -66,13 +74,21 @@ def make_cutflow(samples,
                     cutflow *= weight
                     ds_total *= weight
 
+                if ds.datatype == datasets.DATA:
+                    data_index.append(i)
+
                 total += ds_total
                 if total_cutflow is None:
                     total_cutflow = cutflow
                 else:
                     total_cutflow += cutflow
-        cutflow_table[i] = list(zip(total_cutflow, total_cutflow.yerrh()))
-        cutflow_table[i].insert(0, (total, math.sqrt(total)))
+        if args.errors:
+            cutflow_table[i] = list(zip(total_cutflow, total_cutflow.yerrh()))
+            cutflow_table[i].insert(0, (total, math.sqrt(total)))
+        else:
+            cutflow_table[i] = list(total_cutflow)
+            cutflow_table[i].insert(0, total)
+
 
     filters[0] = 'Skim'
     filters.insert(0, 'Total')
@@ -83,16 +99,31 @@ def make_cutflow(samples,
     if args.format == 'text':
         #print "Integrated luminosity of %.3f fb^-1" % (lumi/1000.)
         sample_names = [sample[1] for sample in samples]
-        table = PrettyTable(['Filter'] + sample_names)
-        if args.short:
-            for i, row in enumerate(zip(*cutflows)):
-                table.add_row([filters[i]] + [num_format % passing for passing, error in row])
-        else:
+        table = TextTable(max_width=-1)
+        #table.set_deco()
+        dtypes = ['t'] + ['f'] * len(cutflows)
+        for i in data_index:
+            dtypes[i + 1] = 'i'
+        table.set_cols_dtype(dtypes)
+        table.set_precision(args.precision)
+        #table.set_cols_align(["l", "r", "r", "r", "l"])
+        #table = PrettyTable(['Filter'] + sample_names)
+        if args.errors:
             for i, row in enumerate(zip(*cutflows)):
                 table.add_row([filters[i]] + ["%s(%s)" % (num_format % passing,
                                                           num_format % error)
                                                           for passing, error in row])
-        print table.get_string(hrules=1)
+        else:
+            table.add_rows([['Filter'] + sample_names] + zip(*([filters] + cutflows)))
+        #table_str = table.get_string(hrules=1)
+        table_str = table.draw()
+        if args.rst:
+            print ".. table::"
+            print "   :class: %s" % args.rst_class
+            print
+            print tab(table_str, 3 * ' ')
+        else:
+            print table_str
     else:
         #print "Integrated luminosity of %.3f fb$^{-1}$\\\\" % (lumi/1000.)
         sample_names = [sample[0] for sample in samples]
