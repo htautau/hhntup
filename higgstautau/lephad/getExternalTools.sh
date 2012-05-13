fc73379a4df045841e9439286f45d72c9a5c366f
Michel Trottier-McDonald <mta58@sfu.ca>
2012-05-13 02:23:04 -0700

    Added script to import external

---
+++ higgstautau/lephad/getExternalTools.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+if[ $USER == "michel"]
+    git clone https://emitc2h@bitbucket.org/emitc2h/external.git
+fi
+
+if[ $USER == "andres"]
+    git clone https://atanasi@bitbucket.org/emitc2h/external.git
+fi
