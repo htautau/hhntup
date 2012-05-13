4d27a00ac73e93d5eb21643aed88da8adbd1b569
Michel Trottier-McDonald <mta58@sfu.ca>
2012-05-13 02:25:47 -0700

    Corrected the getExternalTools.sh script

--- higgstautau/lephad/getExternalTools.sh
+++ higgstautau/lephad/getExternalTools.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-if[ $USER == "michel"]
+if[ $USER -eq "michel"]
     git clone https://emitc2h@bitbucket.org/emitc2h/external.git
 fi
 
-if[ $USER == "andres"]
+if[ $USER -eq "andres"]
     git clone https://atanasi@bitbucket.org/emitc2h/external.git
 fi
