9d868ef04a26cde1e019914f9dbae269269e7a7c
Noel Dawe <Noel.Dawe@cern.ch>
2012-05-17 19:13:36 -0700

    update samples

--- run_mc_hh.py
+++ run_mc_hh.py
@@ -12,7 +12,7 @@ datasets = [
     "PowHegPythia_ggH*_tautauhh.mc11c",
     "PythiaZH*_tautauhh.mc11c",
     "PythiaWH*_tautauhh.mc11c",
-    # background
+    # EW background
     "AlpgenJimmyZtautauNp[0-5]_pt20.mc11c",
     "AlpgenJimmyZtautauNp[0-5]_Mll10to40_pt20.mc11c",
     "AlpgenJimmyWtaunuNp[0-5]_pt20.mc11c",
@@ -22,19 +22,17 @@ datasets = [
     "AlpgenJimmyZeeNp[0-5]_pt20.mc11c",
     "AlpgenJimmyZeeNp[0-5]_Mll10to40_pt20.mc11c",
     "AlpgenJimmyWenuNp[0-5]_pt20.mc11c",
-    "st_tchan_taunu_McAtNlo_Jimmy.mc11c",
-    "st_schan_taunu_McAtNlo_Jimmy.mc11c",
-    "st_Wt_McAtNlo_Jimmy.mc11c",
-    "McAtNlo_JIMMY_WpWm_taunutaunu.mc11c",
-    "gg2WW0240_JIMMY_WW_taunutaunu.mc11c",
-    "McAtNlo_JIMMY_WpZ_taunutautau.mc11c",
-    "McAtNlo_JIMMY_WmZ_taunutautau.mc11c",
-    "McAtNlo_JIMMY_WpZ_qqtautau.mc11c",
-    "McAtNlo_JIMMY_ZZ_4tau.mc11c",
-    "McAtNlo_JIMMY_ZZ_tautaununu.mc11c",
-    "McAtNlo_JIMMY_ZZ_tautauqq.mc11c",
+    # Top
+    "st_*",
+    "AcerMC_Wt.mc11c",
     "T1_McAtNlo_Jimmy.mc11c",
     "TTbar_FullHad_McAtNlo_Jimmy.mc11c",
+    # Diboson
+    "gg2WW0240_JIMMY_WW_*",
+    "McAtNlo_JIMMY_WmZ_*",
+    "McAtNlo_JIMMY_WpWm_*",
+    "McAtNlo_JIMMY_WpZ_*",
+    "McAtNlo_JIMMY_ZZ_",
 ]
 
 cluster.run('HHProcessor.py',
