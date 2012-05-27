e722ed73bc42ec705dde375d21f2252427fb6831
Noel Dawe <Noel.Dawe@cern.ch>
2012-05-27 13:48:00 -0700

    prepare for new skim

--- submit-hhskim.sh
+++ submit-hhskim.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grid-submit -u NoelDawe -m datasets.cfg -s HHSkim.py -v 8 --outputs TPileupReweighting.prw.root mc11_p851_hadhad_theory_uncert
+grid-submit -u DanieleZanzi -m datasets.cfg -s HHSkim.py -v 8 --outputs TPileupReweighting.prw.root mc11_p851_hadhad
