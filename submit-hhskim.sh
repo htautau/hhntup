9a0c58d7f881030069ca2663e4701d32863ab857
Noel Dawe <Noel.Dawe@cern.ch>
2012-05-27 13:37:47 -0700

    skim submission scripts

---
+++ submit-hhskim.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+grid-submit -u NoelDawe -m datasets.cfg -s HHSkim.py -v 8 --outputs TPileupReweighting.prw.root mc11_p851_hadhad_theory_uncert
---
+++ submit-hhskim2.sh
@@ -0,0 +1,1 @@
+grid-submit -u NoelDawe -m datasets.cfg -s HHSkim2.py -v 7 mc11_p851_v7_skim --antiMatch "*TPileupReweighting.prw.root*"
