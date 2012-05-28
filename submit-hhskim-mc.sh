b224914c7b384736fb5b02829474991786ae71b3
Noel Dawe <Noel.Dawe@cern.ch>
2012-05-28 04:36:20 -0700

    updates

--- submit-hhskim-mc.sh
+++ submit-hhskim-mc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grid-submit -u dzanzi -m datasets.cfg -s HHSkim.py -v 8 --outputs TPileupReweighting.prw.root mc11_p851_hadhad
+grid-submit -u dzanzi -m datasets.cfg -s HHSkim.py -v 9 --outputs TPileupReweighting.prw.root mc11_p851_hadhad
