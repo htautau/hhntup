f07ce4001049bb8ecba5efcd0f87bf98cf33049d
Noel Dawe <Noel.Dawe@cern.ch>
2012-05-29 02:39:01 -0700

    updates

--- submit-hhskim2-mc.sh
+++ submit-hhskim2-mc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grid-submit -u dzanzi -m datasets.cfg -s HHSkim2.py -v 1 mc11_p851_v8_skim --antiMatch "*TPileupReweighting.prw.root*"
+grid-submit -u edawe -m datasets.cfg -s HHSkim2.py -v 1 mc11_p851_v10_skim --antiMatch "*TPileupReweighting.prw.root*"
