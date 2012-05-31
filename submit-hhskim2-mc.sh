f7ebbbc62cbf0b5e2d70cf12f2c7a928699e5459
Noel Dawe <Noel.Dawe@cern.ch>
2012-05-31 07:46:09 +0200

    updates

--- submit-hhskim2-mc.sh
+++ submit-hhskim2-mc.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-grid-submit -u edawe -m datasets.cfg -s HHSkim2.py -v 1 mc11_p851_v10_skim --antiMatch "*TPileupReweighting.prw.root*"
+grid-submit -u edawe -m datasets.cfg -s HHSkim2.py -v 1 mc11_p851_v10_skim \
+--antiMatch "*TPileupReweighting.prw.root*" --site SFU-LCG2_LOCALGROUPDISK 
