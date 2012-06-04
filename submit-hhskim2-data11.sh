0ac87f82e376906222cd8c7a99cd583443305592
Noel Dawe <Noel.Dawe@cern.ch>
2012-06-04 01:23:18 -0700

    ignore cutflow.p in old data skims

--- submit-hhskim2-data11.sh
+++ submit-hhskim2-data11.sh
@@ -2,4 +2,5 @@
 
 grid-submit -u group.phys-higgs -m datasets.cfg -s HHSkim2.py -v 1 \
 data11_p851_hadhad_v4_skim \
+--antiMatch "*cutflow.p" \
 --official --voms=atlas:/atlas/phys-higgs/Role=production
