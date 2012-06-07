a5abfea944a2ac71785623bfb275dbcb064e38cb
Noel Dawe <Noel.Dawe@cern.ch>
2012-06-06 22:47:09 -0700

    upgrade distribute version

---
+++ submit-hhskim-embedding11.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+grid-submit -u group.phys-higgs -m datasets.cfg -s HHSkim.py -v 1 \
+embedding11_ztautau_hh_p851 embedding11_ztautau_incl_p851 \
+--official --voms=atlas:/atlas/phys-higgs/Role=production
