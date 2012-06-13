2953a2f46bb6c7f76f966605d433ce1e9aaea834
Noel Dawe <noel.dawe@gmail.com>
2012-06-13 12:52:18 -0700

    embedding samples are at SFU

--- submit-hhskim-embedding11.sh
+++ submit-hhskim-embedding11.sh
@@ -2,4 +2,5 @@
 
 grid-submit -u group.phys-higgs -m datasets.cfg -s HHSkim.py -v 1 \
 embedding11_ztautau_hh_p851 embedding11_ztautau_incl_p851 \
---official --voms=atlas:/atlas/phys-higgs/Role=production
+--official --voms=atlas:/atlas/phys-higgs/Role=production \
+--site ANALY_SFU_bugaboo
