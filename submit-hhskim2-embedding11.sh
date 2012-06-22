a8d8f36bc9a254d781348c477e9644fa241aa2ee
Noel Dawe <Noel.Dawe@cern.ch>
2012-06-22 10:01:30 -0700

    submit skim2 on embedded samples

--- submit-hhskim2-embedding11.sh
+++ submit-hhskim2-embedding11.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 grid-submit -u group.phys-higgs -m datasets.cfg -s HHSkim2.py -v 1 \
-embedding11_ztautau_hh_p851_v1_skim embedding11_ztautau_incl_p851_v1_skim \
---official --voms=atlas:/atlas/phys-higgs/Role=production
+embedding11_ztautau_p851_v1_skim \
+--official --voms=atlas:/atlas/phys-higgs/Role=production \
+--site ANALY_SFU_bugaboo
