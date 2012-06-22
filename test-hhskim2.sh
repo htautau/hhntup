45739ce37baaf04e5f012d8318cceb42383f781a
Noel Dawe <Noel.Dawe@cern.ch>
2012-06-22 06:42:23 -0700

    test script for skim2 of embedded samples

--- test-hhskim2.sh
+++ test-hhskim2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 #grid-batch --dataset Ztautau_hadhad --metadata datasets.cfg --anti-match "*TPileupReweighting.prw.root*" --student HHSkim2 /global/endw/mc11_7TeV/higgs_tautau_hh_reskim_p851/user.NoelDawe.HHSkim.mc11_7TeV.125205.PowHegPythia_VBFH125_tautauhh.merge.NTUP_TAUMEDIUM.e893_s1310_s1300_r3043_r2993_p851.v7.120501172935
-grid-batch --dataset data11_hadhad --metadata datasets.cfg --student HHSkim2 /global/endw/data11_7TeV/higgs_tautau_hh_skim_p851/user.NoelDawe.HTauSkim.data11_7TeV.00191933.physics_JetTauEtmiss.merge.NTUP_TAUMEDIUM.f415_m1025_p851.v4.120204172411
+#grid-batch --dataset data11_hadhad --metadata datasets.cfg --student HHSkim2 /global/endw/data11_7TeV/higgs_tautau_hh_skim_p851/user.NoelDawe.HTauSkim.data11_7TeV.00191933.physics_JetTauEtmiss.merge.NTUP_TAUMEDIUM.f415_m1025_p851.v4.120204172411
+grid-batch --dataset embedding11_ztautau_hh_p851_v1_skim --metadata datasets.cfg --student HHSkim2 /global/endw/embedding11_7TeV/test
