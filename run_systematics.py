336ba3f7ccb71abf5dbf421ce6f7589b8eb16f15
Noel Dawe <noel.dawe@gmail.com>
2012-07-03 06:43:46 -0700

    updates

--- run_systematics.py
+++ run_systematics.py
@@ -4,19 +4,21 @@ import cluster
 SYSTEMATICS_COMMON = [
     ('Jets', 'JES_UP'),
     ('Jets', 'JES_DOWN'),
-]
+    ('Taus', 'TES_UP'),
+    ('Taus', 'TES_DOWN'),
+] # ADD MORE HERE
 
 # hadhad-only systematics
 SYSTEMATICS_HADHAD = [
-]
+] # ADD MORE HERE
 
 # electron-hadron-only systematics
 SYSTEMATICS_EHAD = [
-]
+] # ADD MORE HERE
 
 # muon-hadron-only systematics
 SYSTEMATICS_MUHAD = [
-]
+] # ADD MORE HERE
 
 SYSTEMATICS = {
         'HADHAD': SYSTEMATICS_HADHAD + SYSTEMATICS_COMMON,
