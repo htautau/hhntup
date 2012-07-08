a7868fac2e1bdbcc50d56d1574f1025a63b77f57
Noel Dawe <noel.dawe@gmail.com>
2012-07-08 12:12:46 -0700

    update data run file

--- run_data_hh.py
+++ run_data_hh.py
@@ -7,10 +7,10 @@ import cluster
 setup = cluster.get_setup('setup.noel.sfu.txt')
 
 CWD = os.getcwd()
-NPROC = 5
+NPROC = 1
 NSPLIT = 30
 CMD = "%s && ./run -s HHProcessor.py -n %d --db datasets_hh --nice 10 --split %d:%%d data" % (setup, NPROC, NSPLIT)
 
 for i in xrange(1, NSPLIT + 1):
     cmd = "cd %s && %s" % (CWD, CMD % i)
-    cluster.qsub(cmd, ncpus=NPROC)
+    cluster.qsub(cmd, ncpus=NPROC, queue='short')
