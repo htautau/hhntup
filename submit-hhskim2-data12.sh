cee8bbeafc21d8637f668d514533ddce9403be89
Dugan ONeil <doneil@sfu.ca>
2012-07-14 22:08:00 -0700

    update docs

--- submit-hhskim2-data12.sh
+++ submit-hhskim2-data12.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-grid-submit -u group.phys-higgs -m datasets.cfg -s HHSkim2.py -v 5 \
-data12_p1011_hadhad_v1_skim \
+grid-submit -u group.phys-higgs -m datasets.cfg -s HHSkim2.py -v 1 \
+data12_p1130_hadhad_v1_skim \
 --official --voms=atlas:/atlas/phys-higgs/Role=production
