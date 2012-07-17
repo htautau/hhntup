6647ab2b98f56ee8fd0e57a36c8bd1eb61b8e0d2
Noel Dawe <noel.dawe@gmail.com>
2012-07-17 16:59:51 -0700

    Revert "update skim-2 data12 docs"
    This reverts commit c1796e3fe73f383982b5019915f03043582108ec.

--- submit-hhskim2-data12.sh
+++ submit-hhskim2-data12.sh
@@ -3,4 +3,3 @@
 grid-submit -u group.phys-higgs -m datasets.cfg -s HHSkim2.py -v 1 \
 data12_p1130_hadhad_v1_skim \
 --official --voms=atlas:/atlas/phys-higgs/Role=production
-
--- submit-hhskim2-mc11.sh
+++ submit-hhskim2-mc11.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-grid-submit -u group.phys-higgs -m datasets.cfg -s HHSkim2.py -v 2 \
+grid-submit -u group.phys-higgs -m datasets.cfg -s HHSkim2.py -v 1 \
 mc11_p851_hadhad_v1_skim \
 --antiMatch "*TPileupReweighting.prw.root*" \
 --official --voms=atlas:/atlas/phys-higgs/Role=production
