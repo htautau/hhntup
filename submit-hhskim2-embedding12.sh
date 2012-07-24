fa6cf998559dc08eb3fbcd881aa270442f52d0cb
Dugan ONeil <doneil@sfu.ca>
2012-07-24 09:57:16 -0700

    update embedding files list

---
+++ submit-hhskim2-embedding12.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+grid-submit -u group.phys-higgs -m datasets.cfg -s HHSkim2.py -v 1 \
+embedding12_ztautau_hh_p1130_v1_skim \
+--official --voms=atlas:/atlas/phys-higgs/Role=production \
