747a9877ead1108bade5534cb82409f07487d16c
Noel Dawe <noel.dawe@gmail.com>
2012-07-30 07:24:20 -0700

    updates

--- systematics.py
+++ systematics.py
@@ -31,4 +31,4 @@ def iter_systematics(channel, include_nominal=False):
     if include_nominal:
         yield None
     for sys_variations in SYSTEMATICS[channel.upper()]:
-        yield sys_variations
+        yield set(sys_variations)
