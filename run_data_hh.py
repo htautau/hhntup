748456f671a3967764d6708b45db80c1977f4a0e
Noel Dawe <noel.dawe@gmail.com>
2012-07-30 10:37:58 -0700

    update data run script

--- run_data_hh.py
+++ run_data_hh.py
@@ -3,22 +3,31 @@
 import os
 import cluster
 
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument('--nproc', type=int, default=3)
+parser.add_argument('--nsplit', type=int, default=12)
+parser.add_argument('--queue', default='short')
+parser.add_argument('--nice', type=int, default=10)
+parser.add_argument('--dry', action='store_true', default=False)
+args = parser.parse_args()
 
 setup = cluster.get_setup('setup.noel.sfu.txt')
 
 CWD = os.getcwd()
-NPROC = 1
-NSPLIT = 30
 CMD = ("%s && ./run --output-path ntuples/hadhad "
        "-s HHProcessor.py -n %d --db datasets_hh "
-       "--nice 10 --split %d:%%d data") % (setup, NPROC, NSPLIT)
+       "--nice %d --split %d:%%d data") % (
+               setup, args.nproc, args.nice, args.nsplit)
 
-for i in xrange(1, NSPLIT + 1):
-    cmd = "cd %s && %s" % (CWD, CMD % i)
+for i in xrange(args.nsplit):
+    cmd = "cd %s && %s" % (CWD, CMD % (i + 1))
     cluster.qsub(
         cmd,
-        ncpus=NPROC,
-        name='HHProcessor.data_%d' % i
+        ncpus=args.nproc,
+        name='HHProcessor.data_%d' % (i + 1),
         stderr_path='ntuples/hadhad',
         stdout_path='ntuples/hadhad',
-        queue='short')
+        queue=args.queue,
+        dry_run=args.dry)
