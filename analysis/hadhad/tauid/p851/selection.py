589fb036060dcf4ad1f36dc552539c54b200f549
Noel Dawe <noel.dawe@gmail.com>
2012-08-14 00:44:34 -0700

    updates

---
+++ analysis/hadhad/tauid/p851/selection.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+from rootpy.io import open as ropen
+from rootpy.tree import Cut
+from rootpy.plotting import Graph
+import os
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+
+categories = {
+    '_3': Cut('tau_numberOfVertices<=3'),
+    '3_5': Cut('3<tau_numberOfVertices<=5'),
+    '5_7': Cut('5<tau_numberOfVertices<=7'),
+    '7_': Cut('tau_numberOfVertices>7'),
+}
+
+levels = {
+    'loose': 1,
+    'medium': 2,
+    'tight': 3,
+}
+
+
+if __name__ == '__main__':
+
+    with ropen(os.path.join(HERE, 'bdt_selection.root'), 'recreate') as f:
+
+        for prong in (1, 3):
+            for cat_str, category in categories.items():
+                for level_name, level in levels.items():
+                    fname = 'sig-bits-%dp-%s-perfB--%d.txt' % (
+                            prong, category.safe(parentheses=False), level)
+                    with open(fname) as fin:
+                        lines = fin.readlines()[1:]
+                        graph = Graph(len(lines), name='%s_%dp_%s' % (
+                            level_name, prong, cat_str))
+                        for i, line in enumerate(lines):
+                            pt, bdt = map(float, line.strip().split())
+                            graph[i] = (pt, bdt)
+                        graph.Write()
+else:
+
+    f = ropen(os.path.join(HERE, 'bdt_selection.root'))
+
+    def selection(level, prong, category):
+
+        return f.Get('%s_%dp_%s' % (
+            level, prong, category))
--- analysis/hadhad/tauid_uncertainty.py
+++ analysis/hadhad/tauid_uncertainty.py
@@ -1,3 +1,23 @@
 from samples import MC_Ztautau
+from tauid.p851.selection import selection
 
+"""
+https://twiki.cern.ch/twiki/bin/viewauth/AtlasProtected/TauSystematicsWinterConf2012
+Values are percents
+"""
+EFFIC_UNCERT = {
+    'loose': {
+        1: 4.,
+        3: 8.,
+    },
+    'medium': {
+        1: 5.,
+        3: 8.,
+    },
+    'tight': {
+        1: 4.,
+        3: 7.,
+    },
+}
 
+ztautau = MC_Ztautau()
