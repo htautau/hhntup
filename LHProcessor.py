ca93629d5cefabbb2ce3fee92ef5a87eb462babe
Michel Trottier-McDonald <mta58@sfu.ca>
2012-09-07 01:57:40 -0700

    Preparing for full scale lephad production

--- LHProcessor.py
+++ LHProcessor.py
@@ -156,10 +156,10 @@ class LHProcessor(ATLASStudent):
                 datatype=self.metadata.datatype,
                 verbose=VERBOSE),
             JetPreSelection(),
-            MuonPreSelection(),
             MuonPtSmearing(datatype=self.metadata.datatype),
-            ElectronPreSelection(),
+            MuonPreSelection(),
             EgammaERescaling(datatype=self.metadata.datatype),
+            ElectronPreSelection(),
             JetOverlapRemoval(),
             JetCleaning(self.metadata.datatype, YEAR),
             ElectronLArHole(),
@@ -335,10 +335,10 @@ class LHProcessor(ATLASStudent):
             tree.mass_collinear_tau_lep = collin_mass
             tree.tau_x  = tau_x
             tree.lep_x = lep_x
-            #mmc_mass, mmc_pt, mmc_met = mass.missingmass(Tau, Lep, METx, METy, sumET, leptype)
-            tree.mass_mmc_tau_lep = 0#mmc_mass
-            tree.pt_mmc_tau_lep = 0#mmc_pt.Pt()
-            tree.met_mmc_tau_lep = 0#mmc_met.Mod()
+            mmc_mass, mmc_pt, mmc_met = mass.missingmass(Tau, Lep, METx, METy, sumET, leptype)
+            tree.mass_mmc_tau_lep = mmc_mass
+            tree.pt_mmc_tau_lep = mmc_pt.Pt()
+            tree.met_mmc_tau_lep = mmc_met.Mod()
 
 
 
