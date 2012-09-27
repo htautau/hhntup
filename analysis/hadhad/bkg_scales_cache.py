bffa5dc09a986756aa7c0188c3de92c32daac68d
Noel Dawe <noel.dawe@gmail.com>
2012-09-27 16:57:05 -0700

    make plot comparing fitting methods

--- analysis/hadhad/bkg_scales_cache.py
+++ analysis/hadhad/bkg_scales_cache.py
@@ -92,23 +92,74 @@ if __name__ == '__main__':
 
     parser = ArgumentParser()
     parser.add_argument('--embedding', action='store_true', default=False)
+    parser.add_argument('--plot', action='store_true', default=False)
     parser.add_argument('cache', default='background_scales.cache', nargs='?')
     args = parser.parse_args()
 
+    if args.plot:
+        from matplotlib import pyplot as plt
+
+    z_norms = {}
+    qcd_norms = {}
     read_scales(args.cache)
-    for category in SCALES.keys():
+    categories = sorted(SCALES.keys())
+    for category in categories:
         for embedding in SCALES[category].keys():
             if embedding != args.embedding:
                     continue
-            for param, (qcd_scale, qcd_scale_error,
-                    ztautau_scale, ztautau_scale_error) in \
-                    SCALES[category][embedding].items():
+            params = sorted(SCALES[category][embedding].keys())
+            for param in params:
+                qcd_scale, qcd_scale_error, \
+                ztautau_scale, ztautau_scale_error = \
+                SCALES[category][embedding][param]
+                if param not in z_norms:
+                    z_norms[param] = []
+                    qcd_norms[param] = []
+                z_norms[param].append((ztautau_scale, ztautau_scale_error))
+                qcd_norms[param].append((qcd_scale, qcd_scale_error))
                 print "scale factors for embedding: %s" % str(embedding)
                 print "scale factors for %s category" % category
                 print "fits were derived via %s parameters" % param
                 print "    qcd scale: %.3f +/- %.4f" % (qcd_scale, qcd_scale_error)
                 print "    ztautau scale: %.3f +/- %.4f" % (ztautau_scale, ztautau_scale_error)
                 print
+    if args.plot:
+
+        plt.figure()
+        fig, axs = plt.subplots(nrows=2, ncols=1, sharex=True)
+
+        for param in params:
+            z_n, z_e = zip(*z_norms[param])
+            q_n, q_e = zip(*qcd_norms[param])
+            x = range(len(z_n))
+
+            ax = axs[0]
+            ax.errorbar(x, z_n, z_e, fmt='o',
+                    markersize=5, label='%s Fit' % param)
+            ax = axs[1]
+            ax.errorbar(x, q_n, q_e, fmt='o',
+                    markersize=5, label='%s Fit' % param)
+
+        axs[0].set_ylim(.5, 2)
+        axs[0].set_ylabel('Z Scale Factor')
+
+        axs[1].set_ylim(.5, 2.5)
+        axs[1].set_xticklabels([''] + categories)
+        axs[1].set_xlim(-0.5, len(z_norms[params[0]]) - .5)
+        axs[1].set_ylabel('QCD Scale Factor')
+
+        l1 = axs[0].legend(numpoints=1)
+        l2 = axs[1].legend(numpoints=1)
+
+        l1.get_frame().set_fill(False)
+        l1.get_frame().set_linewidth(0)
+
+        l2.get_frame().set_fill(False)
+        l2.get_frame().set_linewidth(0)
+
+        plt.savefig('bkg_norms.png')
+        plt.savefig('bkg_norms.eps')
+
 else:
     import atexit
 
