9f968dc2768f304ce004560ee2f5b37970d05a0b
Noel Dawe <noel.dawe@gmail.com>
2012-10-10 17:57:04 -0700

    move limits dir to higgstautau-mva

--- limits/hadhad/tools/CINT/addVectorToROOT.C
+++
@@ -1,8 +0,0 @@
-// File loader.C
-#include <vector>
-#include "TLorentzVector.h"
-#ifdef __MAKECINT__
-#pragma link C++ class vector<vector<float> >+;
-#pragma link C++ class vector<vector<int> >+;
-#pragma link C++ class vector<TLorentzVector>;
-#endif
