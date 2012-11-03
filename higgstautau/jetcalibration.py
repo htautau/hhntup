348d795e4a370219e1ba866a75060d59322e104c
Noel Dawe <noel.dawe@gmail.com>
2012-11-03 05:58:42 -0700

    correct 2012 JES

--- higgstautau/jetcalibration.py
+++ higgstautau/jetcalibration.py
@@ -2,6 +2,7 @@
 See instructions here:
     https://twiki.cern.ch/twiki/bin/viewauth/AtlasProtected/HiggsToTauTauToLH2012Summer#Jets_NEW
     https://twiki.cern.ch/twiki/bin/viewauth/AtlasProtected/JetCalibrationToolsWinter2011
+    https://twiki.cern.ch/twiki/bin/viewauth/AtlasProtected/JetCalibrationToolsForPhysicsAnalyses
 """
 from atlastools import datasets
 from rootpy.tree.filtering import EventFilter
@@ -13,7 +14,7 @@ from ROOT import JetCalibrationTool
 
 class JetCalibration(EventFilter):
     """
-    This class applies jet calibrations and allows all events to pass through
+    This class applies jet calibrations
     """
     def __init__(self, datatype, year,
             verbose=False,
@@ -29,15 +30,18 @@ class JetCalibration(EventFilter):
             self.datatype = datatype
             self.isdata = datatype in (datasets.DATA, datasets.EMBED)
             self.verbose = verbose
-            self.algo = "AntiKt4LCTopo"
+            self.algo = 'AntiKt4LCTopo'
             if year == 2011:
-                self.config_file = ApplyJetCalibration.get_resource(
-                        'CalibrationConfigs/InsituJES_2011_Preliminary.config')
+                config = 'InsituJES_2011_Preliminary.config'
+                # use Rel17_JES_AFII.config for fastsim MC
             elif year == 2012:
-                self.config_file = ApplyJetCalibration.get_resource(
-                        'CalibrationConfigs/Rel17_JES.config')
+                config = 'JES_August2012.config'
+                # use JES_August2012_AFII.config for fastsim MC
             else:
                 raise ValueError("No JES calibration defined for year %d" % year)
+            print "Using JES config %s" % config
+            self.config_file = ApplyJetCalibration.get_resource(
+                    'CalibrationConfigs/%s' % config)
             self.jes_tool = JetCalibrationTool(
                     self.algo,
                     self.config_file,
@@ -46,20 +50,10 @@ class JetCalibration(EventFilter):
     def passes(self, event):
 
         # For the pile-up correction, we need mu and NPV(2+ tracks)
-        mu = None
-        if self.datatype == datasets.EMBED:
-            for p in event.mc:
-                if p.pdgId == 39:
-                    mu = p.fourvect.Pz()
-                    break
-        
-            if mu == None:
-                return False
+        mu = event.averageIntPerXing
 
-        else:
-            mu = event.averageIntPerXing
-
-        NPV = 0 # count the number of vertices with 2 or more tracks
+        # count the number of vertices with 2 or more tracks
+        NPV = 0
         for vertex in event.vertices:
             if vertex.nTracks >= 2:
                 NPV += 1
@@ -68,20 +62,37 @@ class JetCalibration(EventFilter):
             print "JETS BEFORE RECALIBRATION"
             self.print_jets(event)
 
-        for jet in event.jets:
-            Eraw    = jet.constscale_E
-            eta_det = jet.constscale_eta
-            eta     = jet.EtaOrigin
-            phi     = jet.PhiOrigin
-            m       = jet.MOrigin
-            # Pile-up, origin, EtaJES correction applied, i.e. to OFFSET_ORIGIN_ETAJES scale
-            calib_jet = self.jes_tool.ApplyOffsetEtaJES(
-                Eraw, eta_det, eta, phi, m, mu, NPV)
-            jet.E = calib_jet.E()
-            jet.m = calib_jet.M()
-            jet.pt = calib_jet.Pt()
-            jet.eta = calib_jet.Eta()
-            jet.phi = calib_jet.Phi()
+        if self.year == 2011:
+            for jet in event.jets:
+                Eraw    = jet.constscale_E
+                eta_det = jet.constscale_eta
+                eta     = jet.EtaOrigin
+                phi     = jet.PhiOrigin
+                m       = jet.MOrigin
+                calib_jet = self.jes_tool.ApplyOffsetEtaJES(
+                    Eraw, eta_det, eta, phi, m, mu, NPV)
+                jet.E = calib_jet.E()
+                jet.m = calib_jet.M()
+                jet.pt = calib_jet.Pt()
+                jet.eta = calib_jet.Eta()
+                jet.phi = calib_jet.Phi()
+
+        elif self.year == 2012:
+            for jet in event.jets:
+                Eraw    = jet.constscale_E
+                eta_det = jet.constscale_eta
+                eta     = jet.constscale_eta
+                phi     = jet.constscale_phi
+                m       = jet.constscale_m
+                calib_jet = self.jes_tool.ApplyOffsetEtaJES(
+                    Eraw, eta_det, eta, phi, m, mu, NPV)
+                jet.E = calib_jet.E()
+                jet.m = calib_jet.M()
+                jet.pt = calib_jet.Pt()
+                jet.eta = calib_jet.Eta()
+                jet.phi = calib_jet.Phi()
+        else:
+            raise ValueError('Invalid year in jet calibration: %d' % self.year)
 
         if self.verbose:
             print "JETS AFTER RECALIBRATION"
