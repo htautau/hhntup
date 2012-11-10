40d19075e8cafad9ce575c188a437c19ff93a0f7
Noel Dawe <noel.dawe@gmail.com>
2012-11-10 13:44:58 -0800

    protect against same data sample being run again

--- run_data_hh.py
+++ run_data_hh.py
@@ -17,16 +17,23 @@ args = parser.parse_args()
 
 setup = cluster.get_setup('setup.noel.sfu.txt')
 
+output_path = 'ntuples/hadhad/HHProcessor'
+
 CWD = os.getcwd()
-CMD = ("%s && ./run --output-path ntuples/hadhad/HHProcessor "
+CMD = ("%s && ./run --output-path %s "
        "-s HHProcessor.py -n %d --db datasets_hh "
        "--nice %d --split %d:%%d data%d-JetTauEtmiss") % (
-               setup, args.nproc, args.nice, args.nsplit, args.year)
+               setup, output_path, args.nproc,
+               args.nice, args.nsplit, args.year)
 
 for i in xrange(args.nsplit):
     if args.splits and (i + 1) not in args.splits:
         continue
     cmd = "cd %s && %s" % (CWD, CMD % (i + 1))
+    output = 'HHProcessor.data%d-JetTauEtmiss_%d.root' % (args.year, i + 1)
+    if os.path.exists(os.path.join(output_path, output)):
+        print "%s already exists. please delete it and resubmit" % output
+        continue
     cluster.qsub(
         cmd,
         ncpus=args.nproc,
