7ce29ecad351b3383c992fa543d7f7e79d3752e3
Noel Dawe <noel.dawe@gmail.com>
2012-11-10 09:23:26 -0800

    split data12 into more subjobs

--- run_all_hh.sh
+++ run_all_hh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ./run_data_hh.py --year 11 --nsplit 40
-./run_data_hh.py --year 12 --nsplit 60
+./run_data_hh.py --year 12 --nsplit 80
 ./run_mc_hh.py --nominal-only --year 11
 ./run_mc_hh.py --nominal-only --year 12
