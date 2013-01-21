3c212e6c41d5850123beda81ef99eee8fbf68f1b
Noel Dawe <noel.dawe@gmail.com>
2013-01-21 05:41:30 -0800

    updates datasets

--- datasets/hadhad/2012/p1344/get_datasets.sh
+++ datasets/hadhad/2012/p1344/get_datasets.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-ami list data --project data12_8TeV --type NTUP_TAU p1344 | sed 's/$/\//g' > data_ami.txt
-dq2-ls data12*physics_JetTauEtmiss*NTUP_TAU.*p1344/ | sort > data_dq2.txt
+
+TAG=p1344
+
+ami list data --project data12_8TeV --type NTUP_TAU $TAG | sed 's/$/\//g' > data_ami.txt
+dq2-ls data12*physics_JetTauEtmiss*NTUP_TAU.*$TAG/ | sort > data_dq2.txt
