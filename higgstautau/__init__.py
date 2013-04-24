0720266169fba1ae01375d026f43769b44c69091
Noel Dawe <Noel.Dawe@cern.ch>
2013-04-24 12:59:59 -0700

    upgrade to new trigger SFs and efficiencies

---
+++ higgstautau/__init__.py
@@ -0,0 +1,12 @@
+import rootpy
+import logging
+
+ROOT.gROOT.SetBatch(True)
+rootpy.log.basic_config_colorized()
+
+log = logging.getLogger('higgstautau')
+if not os.environ.get("DEBUG", False):
+    log.setLevel(logging.INFO)
+
+if hasattr(logging, 'captureWarnings'):
+    logging.captureWarnings(True)
--- higgstautau/trigger/efficiency.py
+++ higgstautau/trigger/efficiency.py
@@ -1,4 +1,5 @@
 import os
+import math
 from operator import itemgetter
 import ROOT
 from rootpy.tree.filtering import *
@@ -6,6 +7,7 @@ from atlastools.units import GeV
 from atlastools import datasets
 
 from ..filters import pileup_vertex_selection
+from . import log; log = log[__name__]
 
 
 class TauTriggerEfficiency(EventFilter):
@@ -19,8 +21,10 @@ class TauTriggerEfficiency(EventFilter):
             return 'periodA'
         elif run <= 209025:
             return 'periodBD'
-        else:
-            return 'periodE'
+        elif run <= 216432:
+            return 'periodEM'
+        # FAIL
+        return None
 
     def __init__(self,
             year,
@@ -92,31 +96,21 @@ class TauTriggerEfficiency(EventFilter):
                     if status != 0:
                         raise RuntimeError(
                             'could not load triggerSF_EF_tau29Ti_medium1.root')
-                    self.passes = self.passes_12_mc
-                """
-                elif datatype == datasets.EMBED:
-                    # use the 3D param
-                    self.corrections = {}
-                    for thresh in (29, 20):
-                        self.corrections[thresh] = {}
-                        for wplevel in ('BDTl', 'BDTm', 'BDTtEVm'):
-                            self.corrections[thresh][wplevel] = {}
-                            for prong in (1, 3):
-                                self.corrections[thresh][wplevel][prong] = {}
-                                tool = ROOT.TauTriggerParameterisation()
-                                tool.loadInputFile(
-                                    os.path.join(base,
-                                        "triggerSF_wmcpara_EF_tau%dTi_medium1.root" %
-                                        thresh),
-                                    "%sP" % prong, wplevel)
-                                self.corrections[thresh][wplevel][prong] = tool
-                    self.passes = self.passes_12_embed
-                """
+                    self.passes = self.passes_12
+                    if datatype == datasets.MC:
+                        self.passes = self.passes_12_mc
+                    else:
+                        self.passes = self.passes_12_embed
                 else:
                     raise ValueError("datatype is not EMBED or MC")
             else:
                 raise ValueError(
                     "No trigger efficiency corrections defined for year %d" % year)
+
+            if datatype == datasets.MC:
+                log.info("will apply MC trigger scale factors")
+            else:
+                log.info("will apply data trigger efficiencies")
 
         super(TauTriggerEfficiency, self).__init__(
                 passthrough=passthrough,
@@ -245,10 +239,8 @@ class TauTriggerEfficiency(EventFilter):
         period = self.get_period_2012(run)
         thresh = []
 
-        if period == 'periodA':
-            eveto = 'EVm'
-        else:
-            eveto = 'EVnone'
+        # new tool only accepts EVnone
+        eveto = 'EVnone'
 
         for tau in event.taus:
 
@@ -270,6 +262,7 @@ class TauTriggerEfficiency(EventFilter):
             elif tau.trigger_match_thresh == 29:
                 ttc = self.ttc_29
                 if tau.pt < 35 * GeV:
+                    log.warning("error in trigger scale factors")
                     # should be rare (swapping of reco vs trigger pT)
                     self.tree.tau_trigger_match_error = True
                 thresh.append(29)
@@ -278,25 +271,43 @@ class TauTriggerEfficiency(EventFilter):
                         % tau.trigger_match_thresh)
 
             for wplevel, wpflag in wp.items():
-                # correct for TES variations (only on nominal)
-                # pt_nominal should equal pt when TES is not applied
                 if self.tes_systematic:
+                    # correct for TES variations (only on nominal)
+                    # pt_nominal should equal pt when TES is not applied
                     #print "%f %f" % (tau.pt, tau.pt_nominal)
                     try:
                         sf = abs(ttc.getSF(tau.pt, tau.eta, 0, period, prong, wpflag, eveto) *
-                            ttc.getMCEff(tau.pt, tau.eta, period, prong, wpflag, eveto) /
-                            ttc.getMCEff(tau.pt_nominal, tau.eta, period, prong, wpflag, eveto))
+                            ttc.getMCEff(tau.pt, tau.eta, 0, period, prong, wpflag, eveto) /
+                            ttc.getMCEff(tau.pt_nominal, tau.eta, 0, period, prong, wpflag, eveto))
                     except ZeroDivisionError:
+                        log.warning(
+                            "division by zero in trigger scale factors: using 1.")
                         sf = 1.
                     setattr(tau, 'trigger_eff_sf_%s' % wplevel, sf)
 
                 else:
-                    setattr(tau, 'trigger_eff_sf_%s' % wplevel,
-                            abs(ttc.getSF(tau.pt, tau.eta, 0, period, prong, wpflag, eveto)))
-                setattr(tau, 'trigger_eff_sf_%s_high' % wplevel,
-                        abs(ttc.getSF(tau.pt, tau.eta, 1, period, prong, wpflag, eveto)))
-                setattr(tau, 'trigger_eff_sf_%s_low' % wplevel,
-                        abs(ttc.getSF(tau.pt, tau.eta, -1, period, prong, wpflag, eveto)))
+                    sf = abs(ttc.getSF(tau.pt, tau.eta, 0, period, prong, wpflag, eveto))
+                    setattr(tau, 'trigger_eff_sf_%s' % wplevel, sf)
+
+                    # MC stat uncert
+                    mc_stat_up = abs(ttc.getSF(tau.pt, tau.eta, 1, period, prong, wpflag, eveto))
+                    mc_stat_dn = abs(ttc.getSF(tau.pt, tau.eta, -1, period, prong, wpflag, eveto))
+                    # Data stat uncert
+                    data_stat_up = abs(ttc.getSF(tau.pt, tau.eta, 2, period, prong, wpflag, eveto))
+                    data_stat_dn = abs(ttc.getSF(tau.pt, tau.eta, -2, period, prong, wpflag, eveto))
+                    # Systematic uncert
+                    sys_up = abs(ttc.getSF(tau.pt, tau.eta, 3, period, prong, wpflag, eveto))
+                    sys_dn = abs(ttc.getSF(tau.pt, tau.eta, -3, period, prong, wpflag, eveto))
+
+                    sigma_up = math.sqrt(math.pow(mc_stat_up, 2.) +
+                                         math.pow(data_stat_up, 2.) +
+                                         math.pow(sys_up, 2.))
+                    sigma_dn = math.sqrt(math.pow(mc_stat_dn, 2.) +
+                                         math.pow(data_stat_dn, 2.) +
+                                         math.pow(sys_dn, 2.))
+
+                    setattr(tau, 'trigger_eff_sf_%s_high' % wplevel, sf + sigma_up)
+                    setattr(tau, 'trigger_eff_sf_%s_low' % wplevel, sf - sigma_dn)
 
         if len(set(thresh)) != 2 or len(thresh) != 2:
             raise Exception("there must be exactly two unique trigger match"
@@ -309,38 +320,46 @@ class TauTriggerEfficiency(EventFilter):
         assert event.taus[0].pt > event.taus[1].pt
         # taus are already sorted in descending order by pT by TauLeadSublead
 
-        npileup_vtx = len([vtx for vtx in event.vertices
-                           if pileup_vertex_selection(vtx)])
+        # get random run number using the pileup tool
+        period = self.get_period_2012(event.RunNumber)
+        # TODO: use:
+        #tool.getPeriodFromRunNumber(event.RunNumber)
+        thresh = []
 
-        for tau, thresh in zip(event.taus, (29, 20)):
+        # new tool only accepts EVnone
+        eveto = 'EVnone'
+
+        for tau, ttc in zip(event.taus, (self.ttc_29, self.ttc_20)):
 
             wp = {}
             wp['loose'] = 'BDTl'
             if tau.JetBDTSigMedium:
                 wp['medium'] = 'BDTm'
             if tau.JetBDTSigTight:
-                wp['tight'] = 'BDTtEVm'
+                wp['tight'] = 'BDTt'
 
             if tau.numTrack > 1:
-                prong = 3
+                prong = '3p'
             else:
-                prong = 1
-
-            if thresh == 29 and tau.pt < 35 * GeV:
-                self.tree.tau_trigger_match_error = True
+                prong = '1p'
 
             for wplevel, wpflag in wp.items():
-                corr = self.corrections[thresh][wpflag][prong]
-                setattr(tau, 'trigger_eff_sf_%s' % wplevel,
-                        abs(corr.get3DMCEff(
-                            tau.pt, tau.eta,
-                            npileup_vtx, 0)))
-                setattr(tau, 'trigger_eff_sf_%s_high' % wplevel,
-                        abs(corr.get3DMCEff(
-                            tau.pt, tau.eta,
-                            npileup_vtx, 1)))
-                setattr(tau, 'trigger_eff_sf_%s_low' % wplevel,
-                        abs(corr.get3DMCEff(
-                            tau.pt, tau.eta,
-                            npileup_vtx, -1)))
+                sf = abs(ttc.getDataEff(tau.pt, tau.eta, 0, period, prong, wpflag, eveto))
+                setattr(tau, 'trigger_eff_sf_%s' % wplevel, sf)
+
+                # Data stat uncert
+                data_stat_up = abs(ttc.getDataEff(tau.pt, tau.eta, 1, period, prong, wpflag, eveto))
+                data_stat_dn = abs(ttc.getDataEff(tau.pt, tau.eta, -1, period, prong, wpflag, eveto))
+                # Systematic uncert
+                sys_up = abs(ttc.getDataEff(tau.pt, tau.eta, 2, period, prong, wpflag, eveto))
+                sys_dn = abs(ttc.getDataEff(tau.pt, tau.eta, -2, period, prong, wpflag, eveto))
+
+                sigma_up = math.sqrt(math.pow(data_stat_up, 2.) +
+                                     math.pow(sys_up, 2.))
+                sigma_dn = math.sqrt(math.pow(data_stat_dn, 2.) +
+                                     math.pow(sys_dn, 2.))
+
+                setattr(tau, 'trigger_eff_sf_%s_high' % wplevel, sf + sigma_up)
+                setattr(tau, 'trigger_eff_sf_%s_low' % wplevel, sf - sigma_dn)
+
         return True
