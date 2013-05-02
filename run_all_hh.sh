e426b0e4caa56142c6b4c30dd9d1ea19f5fec11a
Noel Dawe <Noel.Dawe@cern.ch>
2013-05-01 23:26:07 -0700

    updates

--- run_all_hh.sh
+++ run_all_hh.sh
@@ -4,5 +4,5 @@
 #./run_data_hh.py --year 11 --nsplit 40
 ./run_embed_hh.py --year 12
 #./run_embed_hh.py --nominal-only --year 11
-#./run_mc_hh.py --year 12
+./run_mc_hh.py --year 12
 #./run_mc_hh.py --nominal-only --year 11
