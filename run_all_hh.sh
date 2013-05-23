e26593ac06f5062de98b032f0d6990fd157c9cab
Noel Dawe <Noel.Dawe@cern.ch>
2013-05-23 01:36:59 -0700

    activate a few more JES NPs

--- run_all_hh.sh
+++ run_all_hh.sh
@@ -3,8 +3,8 @@
 ./run_embed_hh.py --nominal-only --year 12 --nsplit 30 --redo-mmc
 ./run_mc_hh.py --nominal-only --year 12 --redo-mmc
 ./run_data_hh.py --year 12 --nsplit 50 --redo-mmc
-#./run_embed_hh.py --systematics-only --year 12 --redo-mmc
-#./run_mc_hh.py --systematics-only --year 12 --redo-mmc
+./run_embed_hh.py --systematics-only --year 12 --redo-mmc
+./run_mc_hh.py --systematics-only --year 12 --redo-mmc
 
 #./run_embed_hh.py --nominal-only --year 11 --redo-mmc
 #./run_mc_hh.py --nominal-only --year 11 --redo-mmc
