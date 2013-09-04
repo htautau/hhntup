c2cc412972b94a440f37ae0a013a44606ae69c10
Noel Dawe <noel.dawe@gmail.com>
2013-09-04 12:34:38 -0700

    updates

--- run_all_hh.sh
+++ run_all_hh.sh
@@ -13,3 +13,7 @@
 #./run_data_hh.py --year 11 --nsplit 40 --redo-mmc
 #./run_embed_hh.py --systematics-only --year 11 --redo-mmc
 #./run_mc_hh.py --systematics-only --year 11 --redo-mmc
+
+./run_mc_hh.py --nominal-only --year 12 --local --redo-selection \
+McAtNloJimmy_AUET2CT10_ggH125_tautau.mc12a \
+PowhegJimmy_AUET2CT10_ggH125_tautauInclusive.mc12a \
