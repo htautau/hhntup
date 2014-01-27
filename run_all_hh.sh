8fde5946f9cdafd7b488727c63514de1a2cb83ba
Noel Dawe <Noel.Dawe@cern.ch>
2014-01-27 00:14:01 -0800

    update datasets

--- run_all_hh.sh
+++ run_all_hh.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#./run_data_hh.py --year 12 --nsplit 50 --local --redo-selection
-#./run_embed_hh.py --nominal-only --year 12 --nsplit 30 --local --redo-selection
-#./run_mc_hh.py --nominal-only --year 12 --local --redo-selection
-#./run_embed_hh.py --systematics-only --year 12 --nsplit 30 --local --redo-selection
-#./run_mc_hh.py --systematics-only --year 12 --local --redo-selection
+./run_data_hh.py --year 12 --nsplit 50 --local
+#./run_embed_hh.py --nominal-only --year 12 --nsplit 30 --local
+#./run_mc_hh.py --nominal-only --year 12 --local
+#./run_embed_hh.py --systematics-only --year 12 --nsplit 30 --local
+#./run_mc_hh.py --systematics-only --year 12 --local
 
-#./run_embed_hh.py --nominal-only --year 11 --redo-mmc
-#./run_mc_hh.py --nominal-only --year 11 --redo-mmc
-#./run_data_hh.py --year 11 --nsplit 40 --redo-mmc
-#./run_embed_hh.py --systematics-only --year 11 --redo-mmc
-#./run_mc_hh.py --systematics-only --year 11 --redo-mmc
+./run_data_hh.py --year 11 --nsplit 40 --local
+#./run_embed_hh.py --nominal-only --year 11 --local
+#./run_mc_hh.py --nominal-only --year 11 --local
+#./run_embed_hh.py --systematics-only --year 11 --local
+#./run_mc_hh.py --systematics-only --year 11 --local
 
 # for theory uncertainty:
 #./run_mc_hh.py --nominal-only --year 12 --local --redo-selection \
