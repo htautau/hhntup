f71d1750abdc98f2af0ee974dc013ffae5c4eb53
Noel Dawe <Noel.Dawe@cern.ch>
2014-02-10 16:34:48 -0800

    use setuptools

--- higgstautau/jetcleaning/setup.py
+++ higgstautau/jetcleaning/setup.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python
 
-from distribute_setup import use_setuptools
-use_setuptools()
-
-from setuptools import setup, find_packages
-from glob import glob
-
-from distutils.core import Extension
+from setuptools import setup, find_packages, Extension
 
 jet_cleaning = Extension('_libcleaning',
-                    sources = ['_libcleaning.cpp',
-                               '_cleaning.cpp'])
+    sources = [
+        '_libcleaning.cpp',
+        '_cleaning.cpp'])
 
 setup(name='higgstautau',
       author='Noel Dawe',
       author_email='noel.dawe@cern.ch',
       packages=find_packages(),
-      ext_modules = [jet_cleaning]
-     )
+      ext_modules = [jet_cleaning])
