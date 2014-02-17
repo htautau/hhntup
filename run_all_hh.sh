2d3ee00d93da412d726c47f85c0f5045c1bbc965
Noel Dawe <Noel.Dawe@cern.ch>
2014-02-17 11:57:01 -0800

    update run script

--- run_all_hh.sh
+++ run_all_hh.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-./run_data_hh.py --year 12 --nsplit 50 --local --redo-selection
-./run_embed_hh.py --nominal-only --year 12 --nsplit 30 --local --redo-selection
-./run_mc_hh.py --nominal-only --year 12 --local --redo-selection
+./run_data_hh.py --year 12 --nsplit 50 --local
+./run_embed_hh.py --nominal-only --year 12 --nsplit 30 --local
+./run_mc_hh.py --nominal-only --year 12 --local
 #./run_embed_hh.py --systematics-only --year 12 --nsplit 30 --local
 #./run_mc_hh.py --systematics-only --year 12 --local
 
-#./run_data_hh.py --year 11 --nsplit 40 --local
-#./run_embed_hh.py --nominal-only --year 11 --nsplit 10 --local
-#./run_mc_hh.py --nominal-only --year 11 --local
+./run_data_hh.py --year 11 --nsplit 40 --local
+./run_embed_hh.py --nominal-only --year 11 --nsplit 10 --local
+./run_mc_hh.py --nominal-only --year 11 --local
 #./run_embed_hh.py --systematics-only --year 11 --local
 #./run_mc_hh.py --systematics-only --year 11 --local
 
