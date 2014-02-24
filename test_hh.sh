216c0d0c0dde33dfcb5b38f6e7c1dfc7a8fd1ad4
Noel Dawe <Noel.Dawe@cern.ch>
2014-02-23 18:13:07 -0800

    updates

--- test_hh.sh
+++ test_hh.sh
@@ -5,5 +5,4 @@
 #AlpgenJimmy_AUET2CTEQ6L1_WtaunuNp3.mc12a $@
 
 source grid.setup && python run --output-path . -s hhskim.py -n 1 \
---db datasets_hh --nice 10 \
-embed12-HH-IM $@
+--db datasets_hh --nice 10 --local embed12-HH-IM $@
