1afe2db215a8f999c0844d62aec469196a50610b
Noel Dawe <Noel.Dawe@cern.ch>
2014-02-25 14:14:45 -0800

    update xsec tables

--- test_hh.sh
+++ test_hh.sh
@@ -4,5 +4,4 @@
 #--db datasets_hh --nice 10 \
 #AlpgenJimmy_AUET2CTEQ6L1_WtaunuNp3.mc12a $@
 
-source grid.setup && python run --output-path . -s hhskim.py -n 1 \
---db datasets_hh --nice 10 --local embed12-HH-IM $@
+source grid.setup && python run --profile --output-path . -s hhskim.py -n 5 --db datasets_hh --nice 10 --suffix TES_TRUE_FINAL_UP AlpJimZtautauNp3_pt20.mc11c --syst-terms TES_TRUE_FINAL_UP --local
