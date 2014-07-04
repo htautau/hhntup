1eb4e3b523947d931d632a8e0d7d219f70a4caa3
Noel Dawe <Noel.Dawe@cern.ch>
2014-07-03 18:22:39 -0700

    clean up mass

--- higgstautau/mass/__init__.py
+++ higgstautau/mass/__init__.py
@@ -1,7 +1,3 @@
 from .. import log; log = log[__name__]
 from .missingmass import MMC
-from .collinearmass import is_MET_bisecting
-from .collinearmass import mass as collinearmass
-from .collinearmass import mass_soshi as collinearmass_alt
-#from .ditaumass import mass as ditaumass
-#from .ditaumass import mass_scan as ditaumass_scan
+from .collinearmass import is_MET_bisecting, mass as collinearmass
